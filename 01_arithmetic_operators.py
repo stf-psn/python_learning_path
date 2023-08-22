15f398f9db5a93e74578336dfa2c6973a274bf88
s.pisano <stefano_pisano@carrefour.com>
2023-08-22 21:54:07 +0200

    added arithmetic and bitwise operator explanation

---
+++ 01_arithmetic_operators.py
@@ -0,0 +1,36 @@
+# print("Hello, world");
+# arithmetic operators
+
+a = 21;
+b = 10;
+c = 0;
+
+c = a + b;
+print(c);
+# output 31
+
+c = a - b;
+print(c);
+# output 11
+
+c = a * b;
+print(c);
+# output 210
+
+c = a / b;
+print(c);
+# output 2.1
+
+c = a % b;
+print(c);
+# output 1
+
+c = b ** 2;
+print(c);
+# output 100
+
+print("The final operation's result was", c)
+
+c = a // b;
+print(c);
+# output is 2. Floor takes the largest integer.
---
+++ 02_bitwise_operators.py
@@ -0,0 +1,23 @@
+# From GeeksforGeeks
+
+# 101   (5 in binario)
+# & 011   (3 in binario)
+#------
+# 001   (Risultato: 1 in decimale)
+
+# A comparison between the two bits is done.
+# A 0 is returned, when using the and bitwise operator, if the bit doesn't match.
+# Since the last bit matches, a 1 is returned.
+
+# 1001   (9 in binario)
+# | 0110   (6 in binario)
+# ------
+#  1111   (Risultato: 15 in decimale)
+# At least one bit was one, during the OR comparison.
+
+# 1010   (10 in binario)
+# ^ 0110   (6 in binario)
+# ------
+#  1100   (Risultato: 12 in decimale)
+
+# During the XOR comparison, if the bits are not the same and at least one bit is one, the value is one, else 0.
